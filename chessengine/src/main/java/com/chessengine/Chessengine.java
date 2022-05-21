6be493a8f6b915ebea34d46f843c1b8e9c0b0d88
Mateus Berger <mateusberger96@gmail.com>
2022-05-21 15:05:57 -0300

    First: Project init

---
+++ chessengine/src/main/java/com/chessengine/Chessengine.java
@@ -0,0 +1,12 @@
+package com.chessengine;
+
+/**
+ *
+ * @author mateu
+ */
+public class Chessengine {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
